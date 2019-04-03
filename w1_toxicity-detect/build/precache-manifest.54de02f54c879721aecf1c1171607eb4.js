f7a1fefd1f45f8ac44ef364693cb7ef2a9dd37a3
vince19972 <vince19972@gmail.com>
2019-04-03 19:20:36 -0400

    :rocket: upload built output

---
+++ w1_toxicity-detect/build/precache-manifest.54de02f54c879721aecf1c1171607eb4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d4f87cd3dd25daef84d",
+    "url": "/static/css/main.80ebc60d.chunk.css"
+  },
+  {
+    "revision": "3d4f87cd3dd25daef84d",
+    "url": "/static/js/main.1d2d0921.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7298f98aaa570a339443",
+    "url": "/static/js/2.be4a8912.chunk.js"
+  },
+  {
+    "revision": "a09e29a659364b302c2302896a824f7d",
+    "url": "/index.html"
+  }
+];
