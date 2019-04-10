c92d5ec38c9315d9e1513309320f62d4eefab228
vince19972 <vince19972@gmail.com>
2019-04-09 22:25:07 -0400

    :rocket: [posenet] add build folder

---
+++ w2_posenet/build/precache-manifest.1bdbc3e441708d25fc5b1245475eaf56.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cfe32d6707d3086e714f",
+    "url": "./static/css/main.606a0f03.chunk.css"
+  },
+  {
+    "revision": "cfe32d6707d3086e714f",
+    "url": "./static/js/main.f4110878.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "f8cc2d6885382c27a509",
+    "url": "./static/js/2.34be42d5.chunk.js"
+  },
+  {
+    "revision": "62f210c2acd86b91183f7a7d88c3a871",
+    "url": "./index.html"
+  }
+];
